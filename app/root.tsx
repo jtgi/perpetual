6e555f7962edba0151c24936bba2f89056eee088
jtgi <j@jtgi.me>
2024-02-25 17:07:17 +0900

    gm

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,32 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { drawClock, frameResponse } from "./utils";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+export async function loader() {
+  const baseUrl = process.env.HOST_URL!;
+
+  const dst = await drawClock(
+    baseUrl + "/fc-perpetual.png",
+    baseUrl + "/hour-silver.png",
+    baseUrl + "/minute-silver.png",
+    baseUrl + "/second-silver.png"
   );
+
+  return frameResponse({
+    title: "title",
+    description: "description",
+    aspectRatio: "1:1",
+    cacheTtlSeconds: 30,
+    image: `${baseUrl}/${dst.replace("public/", "")}`,
+    buttons: [
+      {
+        text: "Mint",
+        link: "https://highlight.xyz/mint/65daea3e14d1e1cf26028d54",
+      },
+    ],
+  });
 }
